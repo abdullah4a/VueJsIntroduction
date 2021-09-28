1d89439a5027bea4942f1e161d78cb882934a9b7
abdullah4a <45167847+abdullah4a@users.noreply.github.com>
2021-09-28 15:00:28 +0500

    Routers

--- src/App.vue
+++ src/App.vue
@@ -1,43 +1,33 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png" />
-    <!-- <bootstrap /> -->
-    <!-- <h1>{{ Data | cur }}</h1> -->
-    <Home />
+    <div id="nav">
+      <router-link to="/">Home</router-link> |
+      <router-link to="/about">About</router-link>|
+      <router-link to="/contactus">Contact Us</router-link>
+    </div>
+    <router-view />
   </div>
 </template>
 
-<script>
-// import bootstrap from "./components/bootstrap.vue";
-import Home from "./components/Home.vue";
-export default {
-  name: "App",
-  components: {
-    // bootstrap,
-    Home,
-  },
-  data() {
-    return {
-      Data: 10000,
-    };
-  },
-};
-</script>
-
 <style lang="scss">
-// $body-bg: #000;
-// $body-color: #111;
-
-// Then import Bootstrap an BootstrapVue SCSS files (order is important)
-@import "node_modules/bootstrap/scss/bootstrap.scss";
-@import "node_modules/bootstrap-vue/src/index.scss";
-@import "~@/assets/scss/vendors/bootstrap-vue/index";
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+}
+
+#nav {
+  padding: 30px;
+
+  a {
+    font-weight: bold;
+    color: #2c3e50;
+
+    &.router-link-exact-active {
+      color: #42f983;
+    }
+  }
 }
 </style>
